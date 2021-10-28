04a5c42fd1fac6d88eb26b3f4abd7a9b4f5010db
C Helder <christian.helder@itvitaelearning.nl>
2021-10-28 15:38:05 +0200

    Add project files.

---
+++ src/GildedRose.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using GildedRose.Business;
+
+namespace GildedRose.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("OMGHAI!");
+
+            ItemService itemService = new ItemService();
+            foreach (var item in itemService.Items)
+            {
+                System.Console.WriteLine($"Name: {item.Name}, Quality: {item.Quality}, Sellin: {item.SellIn}");
+            }
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
