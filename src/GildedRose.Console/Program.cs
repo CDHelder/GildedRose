2303347f7f5d4e0610188aa5eaea9e2990a57567
C Helder <christian.helder@itvitaelearning.nl>
2021-10-28 15:41:51 +0200

    ja

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -14,6 +14,7 @@ namespace GildedRose.Console
                 System.Console.WriteLine($"Name: {item.Name}, Quality: {item.Quality}, Sellin: {item.SellIn}");
             }
 
+
             System.Console.ReadKey();
 
         }
